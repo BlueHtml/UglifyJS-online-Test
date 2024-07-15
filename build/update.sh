8489ad8db8b724d8592788916b5067ed9672f338
fly <luxiaofeng006@qq.com>
2024-07-15 17:16:25 +0800

    use GitHub releases

--- build/update.sh
+++ build/update.sh
@@ -11,41 +11,15 @@ NPM_JSON="build/tmp/npm-uglify-js.json"
 curl --silent --show-error https://registry.npmjs.org/uglify-js > "$NPM_JSON"
 
 VERSION=$(jq -r '."dist-tags".latest' "$NPM_JSON")
-VERSION_GIT_HEAD=$(jq -r '.versions[."dist-tags".latest].gitHead' "$NPM_JSON")
 
 rm -r build/tmp/
 
-echo "Latest version is $VERSION ($VERSION_GIT_HEAD)"
+echo "Latest version is $VERSION"
 
 
-# Update to this version
-git clean -fd
-
-git submodule update --init
-
-cd uglify/
-
-PREV_VERSION=$(jq -r '.version' package.json)
-
-if [ "$VERSION" == "$PREV_VERSION" ]; then
-	echo "Already on the latest version, no update needed"
-	exit 0
-fi
-
-git fetch origin
-if [ $? -ne 0 ]; then
-    echo "Exiting, because it was not possible to fetch remote commits of this submodule"
-    exit 1
-fi
-
-git pull --ff-only origin "$VERSION_GIT_HEAD"
-
-if [ $? -ne 0 ]; then
-    echo "Exiting, because it was not possible to pull this submodule version"
-    exit 1
-fi
-
-cd ..
+# Download this version
+curl -fL "https://github.com/mishoo/UglifyJS/archive/refs/tags/v$VERSION.zip" -o uglify.zip
+unzip -o uglify.zip
 
 
 # Update default options
